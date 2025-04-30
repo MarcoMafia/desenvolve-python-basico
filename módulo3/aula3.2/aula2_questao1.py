07685c19a6d64fcdd27df60d9d38ebe3c29aeeb3
MarcoMafia <166869200+MarcoMafia@users.noreply.github.com>
2025-04-30 08:45:22 -0300

    Add files via upload

---
+++ módulo3/aula3.2/aula2_questao1.py
@@ -0,0 +1,4 @@
+idade_juliana=int(input("Qual é a sua idade? "))
+idade_chris=int(input("Qual é a sua idade? "))
+maior17 = idade_juliana > 17 and idade_cris > 17
+print(maior17)
---
+++ módulo3/aula3.2/aula2_questao2.py
@@ -0,0 +1,4 @@
+idade_juliana=int(input("Qual é a sua idade? "))
+idade_chris=int(input("Qual é a sua idade? "))
+maior17 = idade_juliana > 18 or idade_cris > 18
+print(maior17)
