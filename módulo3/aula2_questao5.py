e931263179e1d65b55718d85a336a1498604c3f2
MarcoMafia <166869200+MarcoMafia@users.noreply.github.com>
2025-04-30 08:45:58 -0300

    Delete módulo3/aula2_questao5.py

--- módulo3/aula2_questao5.py
+++
@@ -1,7 +0,0 @@
-genero= input ("Qual o seu gênero? ")
-idade = int(input("Qual a sua idade? "))
-tempo_servico= int(input("Qual o seu tempo de serviço, em anos? "))
-aposentado= ((genero == "Feminino" and idade > 60) or (genero == "Masculino" and idade > 65) or (tempo_servico >= 30) or ((idade >= 60 or idade < 65 ) and tempo_servico >= 25))
-if aposentado == True: 
-    print("Você já pode se aposentar") 
-else: print ("Você ainda não pode se aposentar")
